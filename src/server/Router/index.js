3f7dd944e3de2681f4c6f8bc6817f35a841383d9
zhuyucai <383326809@qq.com>
2017-10-11 10:54:57 +0800

    fix eslint

--- src/server/Router/index.js
+++ src/server/Router/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const User = require('../Models/users')
 
-router.post('/login', function (req, res) {
+router.post('/login', function(req, res) {
   const postData = {
     username: req.body.username,
     password: req.body.password
@@ -10,7 +10,7 @@ router.post('/login', function (req, res) {
   User.findOne({
     username: postData.username,
     password: postData.password
-  }, function (err, data) {
+  }, function(err, data) {
     if (err) throw err
     if (data) {
       res.json({ status: '1', msg: '登录成功' })
@@ -20,20 +20,20 @@ router.post('/login', function (req, res) {
   })
 })
 
-router.post('/regist', function (req, res) {
+router.post('/regist', function(req, res) {
   // 获取用户提交的信息
   const postData = {
     username: req.body.username,
     password: req.body.password
   }
   // 查询是否被注册
-  User.findOne({username: postData.username}, function (err, data) {
+  User.findOne({username: postData.username}, function(err, data) {
     if (err) throw err
     if (data) {
       res.json({ status: '0', errorMsg: '用户名已被注册' })
     } else {
       // 保存到数据库
-      User.create(postData, function (err, data) {
+      User.create(postData, function(err, data) {
         if (err) throw err
         res.json({ status: '1', msg: '注册成功' })
       })
